a47be58ea60081ec9f426dc87541b99eb79426d4
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-03-28 14:05:31 +0200

    Assign Preferences to Guardians

--- index.js
+++ index.js
@@ -30,6 +30,9 @@ const guardians = {
     "Star-Lord": "Rock",
     "Gamora": "Pop",
     // Add preferences for Drax, Rocket, and Groot
+    "Drax": "R&B",
+    "Rocket": "Amapiano",
+    "Groot": "Afrobeats"
 };
 
 // Function to generate playlist based on preferred genre
